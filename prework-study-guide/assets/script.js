36e71875c369245e694a035e17009baa7e4f7255
MacKetchup <stylesthestyer@gmail.com>
2022-12-05 22:42:02 -0700

    added first for loop of javascript code to study guide!

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!")
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!")
+} else if (topic === 'Git') {
+    console.log("Lets Study Git!")
+} else if (topic === 'Javascript') {
+    console.log("Let's study Javascript")
+} else {
+    console.log('Please try again!');
+}
+*/
